9865188fd8967251e46aacc87f92be2b0b978700
Alexa Summers <alexas0202@gmail.com>
2022-04-12 07:27:23 -0500

    Updates
    Add updated pages

--- public/script.js
+++ public/script.js
@@ -4,6 +4,9 @@ $( document ).ready( function() {
     $( "#BTN_EXA" ).click( function() { $("#WIN_EXA").fadeIn( "fast", function(){} ); } );
     $( "#BTN_EXP" ).click( function() { $("#WIN_EXP").fadeIn( "fast", function(){} ); } );
     $( "#BTN_CON" ).click( function() { $("#WIN_CON").fadeIn( "fast", function(){} ); } );
+    $( "#BTN_CV" ).click( function() { $("#WIN_CV").fadeIn( "fast", function(){} ); } );
+    $( "#BTN_BKG" ).click( function() { $("#WIN_BKG").fadeIn( "fast", function(){} ); } );
+    $( "#BTN_DIV" ).click( function() { $("#WIN_DIV").fadeIn( "fast", function(){} ); } );
     
     $( ".BTN_CLOSE" ).click( function() {
       $( this ).closest( ".window" ).fadeOut( "fast", function() {} );
