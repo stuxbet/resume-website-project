aab990f1eb23c2f293c26b17f7d334b0009bc586
Alexa Summers <alexas0202@gmail.com>
2022-04-15 12:55:03 -0500

    Update JS for buttons

--- public/script.js
+++ public/script.js
@@ -1,13 +1,15 @@
 $( document ).ready( function() {
     $( "#BTN_WEL" ).click( function() { $("#WIN_WEL").fadeIn( "fast", function(){} ); } );
     $( "#BTN_TEA" ).click( function() { $("#WIN_TEA").fadeIn( "fast", function(){} ); } );
-    $( "#BTN_EXA" ).click( function() { $("#WIN_EXA").fadeIn( "fast", function(){} ); } );
-    $( "#BTN_EXP" ).click( function() { $("#WIN_EXP").fadeIn( "fast", function(){} ); } );
+    $( "#BTN_EXA" ).click( function() { $("#WIN_SCH").fadeIn( "fast", function(){} ); } );
+    $( "#BTN_EXP" ).click( function() { $("#WIN_SER").fadeIn( "fast", function(){} ); } );
     $( "#BTN_CON" ).click( function() { $("#WIN_CON").fadeIn( "fast", function(){} ); } );
     $( "#BTN_CV" ).click( function() { $("#WIN_CV").fadeIn( "fast", function(){} ); } );
     $( "#BTN_BKG" ).click( function() { $("#WIN_BKG").fadeIn( "fast", function(){} ); } );
     $( "#BTN_DIV" ).click( function() { $("#WIN_DIV").fadeIn( "fast", function(){} ); } );
-    
+    $( "#BTN_SAM" ).click( function() { $("#WIN_SAM").fadeIn( "fast", function(){} ); } );
+
+
     $( ".BTN_CLOSE" ).click( function() {
       $( this ).closest( ".window" ).fadeOut( "fast", function() {} );
     } );
