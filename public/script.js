7e6e51ec714b5b0069ab0ff8ee96cf4734a54295
Alexa Summers <alexas0202@gmail.com>
2022-04-15 12:55:59 -0500

    Update JS for buttons

--- public/script.js
+++ public/script.js
@@ -1,8 +1,8 @@
 $( document ).ready( function() {
     $( "#BTN_WEL" ).click( function() { $("#WIN_WEL").fadeIn( "fast", function(){} ); } );
     $( "#BTN_TEA" ).click( function() { $("#WIN_TEA").fadeIn( "fast", function(){} ); } );
-    $( "#BTN_EXA" ).click( function() { $("#WIN_SCH").fadeIn( "fast", function(){} ); } );
-    $( "#BTN_EXP" ).click( function() { $("#WIN_SER").fadeIn( "fast", function(){} ); } );
+    $( "#BTN_SCH" ).click( function() { $("#WIN_SCH").fadeIn( "fast", function(){} ); } );
+    $( "#BTN_SER" ).click( function() { $("#WIN_SER").fadeIn( "fast", function(){} ); } );
     $( "#BTN_CON" ).click( function() { $("#WIN_CON").fadeIn( "fast", function(){} ); } );
     $( "#BTN_CV" ).click( function() { $("#WIN_CV").fadeIn( "fast", function(){} ); } );
     $( "#BTN_BKG" ).click( function() { $("#WIN_BKG").fadeIn( "fast", function(){} ); } );
