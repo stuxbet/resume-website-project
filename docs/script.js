fae231d983d6e5c30cff1d61ea0bd0259c8dcaed
luke <lhm9cb@umsystem.edu>
2024-07-14 19:27:04 -0500

    fix

--- docs/script.js
+++ docs/script.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
   const sections = [
-      "#WIN_WEL", "#WIN_TEA", "#WIN_SCH", "#WIN_SER", 
-      "#WIN_CON", "#WIN_CV", "#WIN_BKG", "#WIN_DIV", 
-      "#WIN_SAM", "#WIN_PHIL"
+      "#WIN_WEL", "#WIN_CV", "#WIN_BKG", "#WIN_SK", "#WIN_CON"
   ];
 
   function showSection(sectionToShow) {
@@ -16,15 +14,10 @@ $(document).ready(function() {
   }
 
   $("#BTN_WEL").click(function() { showSection("#WIN_WEL"); });
-  $("#BTN_TEA").click(function() { showSection("#WIN_TEA"); });
-  $("#BTN_SCH").click(function() { showSection("#WIN_SCH"); });
-  $("#BTN_SER").click(function() { showSection("#WIN_SER"); });
-  $("#BTN_CON").click(function() { showSection("#WIN_CON"); });
   $("#BTN_CV").click(function() { showSection("#WIN_CV"); });
   $("#BTN_BKG").click(function() { showSection("#WIN_BKG"); });
-  $("#BTN_DIV").click(function() { showSection("#WIN_DIV"); });
-  $("#BTN_SAM").click(function() { showSection("#WIN_SAM"); });
-  $("#BTN_PHIL").click(function() { showSection("#WIN_PHIL"); });
+  $("#BTN_SK").click(function() { showSection("#WIN_SK"); });
+  $("#BTN_CON").click(function() { showSection("#WIN_CON"); });
 
   $(".BTN_CLOSE").click(function() {
       $(this).closest(".window").fadeOut("fast");
