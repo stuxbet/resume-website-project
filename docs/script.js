8b9ec9a60d2472f8b573c344d06b9b797a721699
luke <lhm9cb@umsystem.edu>
2024-07-14 19:47:41 -0500

    fix

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const sections = [
-      "#WIN_WEL", "#WIN_CV", "#WIN_BKG", "#WIN_SK", "#WIN_CON"
+      "#WIN_WEL", "#WIN_CV", "#WIN_BKG", "#WIN_SK", "#WIN_CON", "#WIN_PG", "#WIN_WK"
   ];
 
   function showSection(sectionToShow) {
@@ -18,6 +18,8 @@ $(document).ready(function() {
   $("#BTN_BKG").click(function() { showSection("#WIN_BKG"); });
   $("#BTN_SK").click(function() { showSection("#WIN_SK"); });
   $("#BTN_CON").click(function() { showSection("#WIN_CON"); });
+  $("#BTN_WK").click(function() { showSection("#WIN_WK"); });
+  $("#BTN_PG").click(function() { showSection("#WIN_PG"); });
 
   $(".BTN_CLOSE").click(function() {
       $(this).closest(".window").fadeOut("fast");
